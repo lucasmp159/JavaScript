3aac6f6c910018dca236c22122da3f01991dab1e
Lucas Moreira <71292751+lucasmp159@users.noreply.github.com>
2023-04-27 17:00:32 -0300

    Add files via upload

--- React/App.js
+++ React/App.js
@@ -3,6 +3,7 @@ import style from "./App.css"
 function App() {
   return (
     <div className="App">
+      <div className="container"></div>
       <h1 className="title">Hello World</h1>
     </div>
   );
