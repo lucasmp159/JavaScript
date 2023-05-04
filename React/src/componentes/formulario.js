6a2385e7176b80fb4e68a49fb4d6721fb1ab45e9
Lucas Moreira <71292751+lucasmp159@users.noreply.github.com>
2023-05-04 16:07:21 -0300

    Create formulario.js

---
+++ React/src/componentes/formulario.js
@@ -0,0 +1,12 @@
+import './formulario.css'
+
+const Formulario = () => {
+    return (
+        <div className="form">
+            <label>Nome</label>
+            <input placeholder="Digite seu nome"/>
+        </div>
+    )
+}
+
+export default Formulario;
