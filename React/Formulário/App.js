8dd200b7f97e116c558afe93c7bace325ddc0365
Lucas Moreira <71292751+lucasmp159@users.noreply.github.com>
2023-05-11 17:25:28 -0300

    Add files via upload

---
+++ React/Formulário/App.js
@@ -0,0 +1,13 @@
+import Formulario_final from "./componentes/formulario_final/formulario_final";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Formulario_final />
+
+    </div>
+  );
+}
+
+export default App;
